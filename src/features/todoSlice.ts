56640a6c648f58f0baac055b6db3131a8515fdc2
Abdeldjalil Hachimi <abdeldjalil_hachimi@hotmail.com>
2021-12-09 22:04:53 +0100

    Adding new funtionalities and validate the todo form

---
+++ src/features/todoSlice.ts
@@ -0,0 +1,45 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Todo } from "../models/Todo.model";
+import { v4 as uuidv4 } from "uuid";
+
+const initialState = [] as Todo[];
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    //add title for todo
+    addTodo: {
+      reducer: (state, action: PayloadAction<Todo>) => {
+        state.push(action.payload);
+      },
+      prepare: (title: string, description: string) => ({
+        payload: {
+          id: uuidv4(),
+          title,
+          description,
+          completed: false,
+        } as Todo,
+      }),
+    },
+    editTodo(state, action: PayloadAction<string>) {
+      const index = state.findIndex((todo) => todo.id === action.payload);
+      state.splice(index, 1);
+    },
+    removeTodo(state, action: PayloadAction<string>) {
+      const index = state.findIndex((todo) => todo.id === action.payload);
+      state.splice(index, 1);
+    },
+    setTodoStatus(
+      state,
+      action: PayloadAction<{ completed: boolean; id: string }>
+    ) {
+      const index = state.findIndex((todo) => todo.id === action.payload.id);
+      state[index].completed = action.payload.completed;
+    },
+  },
+});
+
+export const { addTodo, editTodo, removeTodo, setTodoStatus } =
+  todoSlice.actions;
+export default todoSlice.reducer;
